2511f5f953d1242191d35747132cd48352d8dce7
Muhammad Ali <alihere786@example.com>
2026-02-26 17:52:03 +0500

    Basic Files Struture, Libs Installed

---
+++ src/constants/fonts.js
@@ -0,0 +1,10 @@
+// fonts.js - Roboto fonts
+const fonts = {
+  regular: 'Roboto-Regular',
+  bold: 'Roboto-Bold',
+  semiBold: 'Roboto-Medium', // Roboto uses Medium for semibold
+  extraBold: 'Roboto-Black', // Roboto uses Black for extra bold
+  italic: 'Roboto-Italic', // Regular italic
+};
+
+export default fonts;
